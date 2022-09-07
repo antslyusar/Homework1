103ff684d5ee8b0a9972223d74711debe9b574f3
Anton <antslyusar@gmail.com>
2022-09-07 19:43:02 +0700

    Fourth task

---
+++ HW1_Task8/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Enter a number and explore which even numbers are in the range");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 2; i <= N; i += 2)
+    Console.WriteLine(i);
