ffe5ceeb0cc3bc92d77650b658ec34f29163f177
Anton <antslyusar@gmail.com>
2022-09-07 18:13:03 +0700

    First task

---
+++ HW1_Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter 2 numbers and explore which one is larger!");
+int a, b;
+Console.WriteLine("Enter the firts number - ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the second number - ");
+b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine($"{a} larger than {b}");
+}
+else
+{
+    Console.WriteLine($"{b} larger than {a}");
+}
