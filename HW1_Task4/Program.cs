64052734b9fe67a3f121e1b498212aeeb45f683d
Anton <antslyusar@gmail.com>
2022-09-07 18:29:41 +0700

    Second task

---
+++ HW1_Task4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter 3 numbers and explore which one is the largest!");
+int a, b, c;
+Console.WriteLine("Enter the firts number - ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the second number - ");
+b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the third number - ");
+c = Convert.ToInt32(Console.ReadLine());
+
+int max = (Math.Max(a, Math.Max(b, c)));
+Console.WriteLine($"The largest number is {max}");
