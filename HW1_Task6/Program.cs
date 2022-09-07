79307bdeaa357fbb0bb5363e8d9205eca4ffb746
Anton <antslyusar@gmail.com>
2022-09-07 19:08:32 +0700

    Third task

---
+++ HW1_Task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter a number and explore if it is even or odd!");
+int a;
+a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine($"{a} is even");
+}
+else
+{
+    Console.WriteLine($"{a} is odd");
+}
