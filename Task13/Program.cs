36122b9879d17789878d95ee9edc7c23b74f1661
Anton <antslyusar@gmail.com>
2022-09-15 15:18:13 +0700

    Thirteenth task

---
+++ Task13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter any number an find out which digit is third");
+int a = Convert.ToInt32(Console.ReadLine());
+string aString = Convert.ToString(a);
+if (aString.Length > 2)
+{
+    Console.WriteLine($"The third digit is {aString[2]}");
+}
+else
+{
+    Console.WriteLine("There is no third digit");
+}
