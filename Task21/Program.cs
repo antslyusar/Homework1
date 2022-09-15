307c251c80ac6594a287786d09163bbe19848e8a
Anton <antslyusar@gmail.com>
2022-09-15 17:17:27 +0700

    Twenty first task

---
+++ Task21/Program.cs
@@ -0,0 +1,25 @@
+﻿int x1 = Coordinate("X", "A");
+int y1 = Coordinate("Y", "A");
+int z1 = Coordinate("Z", "A");
+int x2 = Coordinate("X", "B");
+int y2 = Coordinate("Y", "B");
+int z2 = Coordinate("Z", "B");
+
+int Coordinate(string coorName, string pointName)
+{
+    Console.Write($"Enter the {coorName} coordinate of point {pointName}: ");
+    return Convert.ToInt16(Console.ReadLine());
+}
+
+double Decision(double x1, double x2,
+                double y1, double y2,
+                double z1, double z2)
+{
+    return Math.Sqrt(Math.Pow((x2 - x1), 2) +
+                     Math.Pow((y2 - y1), 2) +
+                     Math.Pow((z2 - z1), 2));
+}
+
+double segmentLength = Math.Round(Decision(x1, x2, y1, y2, z1, z2), 2);
+
+Console.WriteLine($"The length of the segment is {segmentLength}");
