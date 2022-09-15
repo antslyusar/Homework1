935e45ebb9f72a23f053e6fa21ea98f3666dc806
Anton <antslyusar@gmail.com>
2022-09-15 14:52:04 +0700

    Tenth task

---
+++ HW1_Task10/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Enter three-digit number: ");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeDigitNumber);
+Console.WriteLine("The second digit is " + stringNumber[1]);
