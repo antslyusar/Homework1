757c95f4c4165252c0bf0c13085ccd48858a4ef3
Anton <antslyusar@gmail.com>
2022-09-15 17:06:14 +0700

    Nineteenth task

---
+++ Task19/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter a five-digit number and find out if it is a palindrome: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int n1 = n / 10000;
+int n2 = n / 1000 % 10;
+int n4 = n / 10 % 10;
+int n5 = n % 10;
+
+if (n1 == n5 && n2 == n4)
+{
+    Console.WriteLine("Yes");
+}
+else Console.WriteLine("No");
