f58953fd6bb2d77ca50339547c0cdbcd3a82d690
Anton <antslyusar@gmail.com>
2022-09-15 15:34:32 +0700

    Fifteenth task

---
+++ Task15/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter a number corresponding to the day of the week and find out which one is a day off ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+if (dayNumber == 6 || dayNumber == 7)
+{
+    Console.WriteLine("Yes");
+}
+else Console.WriteLine("No");
